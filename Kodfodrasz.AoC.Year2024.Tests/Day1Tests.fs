b04002627cc5ebeb005ae7bb56783b54e5b9fa1a
Gábor Gergely <ggergely@kodfodrasz.net>
2024-12-04 23:43:07 +0100

    day 1

--- Kodfodrasz.AoC.Year2024.Tests/Day1Tests.fs
+++ Kodfodrasz.AoC.Year2024.Tests/Day1Tests.fs
@@ -8,24 +8,43 @@ open Kodfodrasz.AoC.Year2024.Day1
 
 
 let exampleInput = """
-TODO
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
   """
 
-[<Fact(Skip="TODO")>]
+[<Fact>]
 let ``Parsing example input`` () =
-  let expected: Result<string list, string> = Ok [ 
-    "TODO";
+  let expected: Result<(int*int) list, string> = Ok [ 
+      3, 4;
+      4, 3;
+      2, 5;
+      1, 3;
+      3, 9;
+      3, 3;
   ]
 
   test
     <@ let actual = parseInput exampleInput
        actual = expected @>
 
-[<Fact(Skip="TODO")>]
+[<Fact>]
 let ``Answer 1 for example input`` () =
   let input = parseInput exampleInput
 
   test
     <@ let actual = Result.bind answer1 input
-       let expected: Result<int, string> = Ok 142
+       let expected: Result<int, string> = Ok 11
+       actual = expected @>
+
+[<Fact>]
+let ``Answer 2 for example input`` () =
+  let input = parseInput exampleInput
+
+  test
+    <@ let actual = Result.bind answer2 input
+       let expected: Result<int, string> = Ok 31
        actual = expected @>
--- Kodfodrasz.AoC.Year2024/Day1.fs
+++ Kodfodrasz.AoC.Year2024/Day1.fs
@@ -4,24 +4,43 @@ open System
 open Kodfodrasz.AoC
 
 
-let parseInput (input: string): Result<string list, string> =
+let parseInput (input: string): Result<'a list, string> =
   input.Split('\n')
-  |> Seq.map String.trim
-  |> Seq.skipWhile String.isNullOrEmpty
-  // no transformation
-  |> Seq.where String.notNullOrWhiteSpace
+  |> Seq.choose (fun line -> 
+    line.Split([|' '; '\t'|],
+     StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries)
+      |> Array.map Parse.parseInt
+      |> function
+      | [| Some fst ; Some  snd  |] -> Some (fst, snd)
+      | _ -> None
+  )
   |> Seq.toList
   |> Ok 
 
-let answer1 (strings: string list) : Result<int,string> =
-  Error "TODO"
+let answer1 (rows: (int*int) list) : Result<int,string> =
+  let arr1, arr2  = List.unzip rows
+  (List.sort arr1, List.sort arr2)
+  ||> Seq.zip
+  |> Seq.map (fun (a,  b) -> abs(b-a) )
+  |> Seq.sum
+  |> Ok
 
+let answer2 (rows: (int*int) list) : Result<int,string>  =
+  let arr1, arr2  = List.unzip rows
+  let arr2counts = List.groupBy id arr2 |> Map.ofList
+  
+  let eval map (key:int) =
+    Map.tryFind key map
+    |> Option.map List.length 
+    |> Option.defaultValue 0
+    |> ((*) key) 
 
-let answer2  (strings: string list)  : Result<int,string>  =
-  Error "TODO"
+  arr1
+  |> List.sumBy( eval arr2counts )
+  |> Ok
 
 type Solver() =
-  inherit SolverBase("Trebuchet?!")
+  inherit SolverBase("Historian Hysteria")
   with
     override this.Solve input =
       this.DoSolve parseInput [ answer1; answer2 ] input
